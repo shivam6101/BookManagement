90b018c86c8268adf57bff38c74718048d90bd3f
ARTI DHOLE <93571821+ArtiDholeSD@users.noreply.github.com>
2022-05-23 15:54:10 +0530

    adding some files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,71 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ListOrderComponent } from './order/list-order/list-order.component';
+import { OrderModule } from './order/order.module';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { ViewAllProductComponent } from './products/view-all-product/view-all-product.component';
+import { ViewProductByCategaryComponent } from './products/view-product-by-categary/view-product-by-categary.component';
+import { ViewProductComponent } from './products/view-product/view-product.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    //component: user books show page
+    redirectTo: 'list-product',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'order',
+    component: ListOrderComponent,
+  },
+  {
+    path: 'view-product',
+    component: ViewProductComponent,
+  },
+  {
+    path: 'add-product',
+    component: AddProductComponent,
+  },
+  {
+    path: 'list-product',
+    component: ViewAllProductComponent,
+  },
+  {
+    path: 'list-product-byCategory',
+    component: ViewProductByCategaryComponent,
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialComponentsModule } from './material-components/material-components.module';
+import { CardComponent } from './card/card.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OrderModule } from './order/order.module';
+import { DashboardLayoutModule } from './dashboard-layout/dashboard-layout.module';
+import { ProductsModule } from './products/products.module';
+//import { ViewAllProductComponent } from './products/view-all-product/view-all-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialComponentsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    OrderModule,
+    DashboardLayoutModule,
+    ProductsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
